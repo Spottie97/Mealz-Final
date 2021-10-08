ec85ac77ecfd9a890ec4c10fe00da672caab6bc0
Tomei Fourie <91941149+tomei123@users.noreply.github.com>
2021-10-08 21:30:29 +0200

    Create frmTransactions.cs
    getTransactions( ) fetches the students from the database and adds them to the textBox.
    
    How are you going to pass the student's id from the previous form to the transactions form?

---
+++ Mealz Demo/frmTransactions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mealz_Demo
+{
+    public partial class frmTransactions : Form
+    { 
+        public frmTransactions(string studentName)
+        {
+            InitializeComponent();
+        }
+
+        private void frmTransactions_Load(object sender, EventArgs e)
+        {
+            string student_id = "";
+            label1.Text = "studentName's transactions";
+
+            string sqlConnectionString = ""; // Replace "" with the connection string to your database.
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionString); // Replace "" with sqlConnectionString
+
+            sqlConnection.Open();
+
+            string selectTransactions = "SELECT * FROM table WHERE student_id=@student_id";
+            SqlCommand sqlCommand = new SqlCommand(selectTransactions, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@student_id", student_id);
+
+            SqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    /*
+                       GetString(index)
+
+                        Add a dataReader.GetString(index) for every single column that you want to display in the textBox.
+                     */
+
+                    string transaction = "\n" + dataReader.GetString(0) + "\t" + dataReader.GetString(0);
+                    textBox1.AppendText(transaction);
+                }
+            }
+
+            dataReader.Close();
+
+            sqlConnection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
