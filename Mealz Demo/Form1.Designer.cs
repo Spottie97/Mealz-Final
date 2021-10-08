8ead9206a1f034b705bcfbf8966b93b392bee7f5
Spottie97 <65610257+Spottie97@users.noreply.github.com>
2021-10-08 18:17:28 +0200

    Login Form Layout Done!

--- Mealz Demo/Form1.Designer.cs
+++ Mealz Demo/Form1.Designer.cs
@@ -29,101 +29,104 @@ namespace Mealz_Demo
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pbLoginPic1 = new System.Windows.Forms.PictureBox();
+            this.tbLoginPass = new System.Windows.Forms.TextBox();
+            this.tbLoginStudentNum = new System.Windows.Forms.TextBox();
+            this.lblLoginStudentNum = new System.Windows.Forms.Label();
+            this.pbLoginPic2 = new System.Windows.Forms.PictureBox();
+            this.lblLoginPass = new System.Windows.Forms.Label();
+            this.lblLoginForgetPass = new System.Windows.Forms.Label();
+            this.lblLoginCreateAcc = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLoginPic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLoginPic2)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnLogin
             // 
-            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.button1.Location = new System.Drawing.Point(263, 295);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(176, 53);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnLogin.Location = new System.Drawing.Point(263, 295);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(176, 53);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
             // 
-            // pictureBox1
+            // pbLoginPic1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(231, 247);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.pbLoginPic1.Image = global::Mealz_Demo.Properties.Resources.Mealz_logos__2_;
+            this.pbLoginPic1.Location = new System.Drawing.Point(12, 12);
+            this.pbLoginPic1.Name = "pbLoginPic1";
+            this.pbLoginPic1.Size = new System.Drawing.Size(231, 247);
+            this.pbLoginPic1.TabIndex = 1;
+            this.pbLoginPic1.TabStop = false;
             // 
-            // textBox1
+            // tbLoginPass
             // 
-            this.textBox1.Location = new System.Drawing.Point(263, 162);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(176, 23);
-            this.textBox1.TabIndex = 3;
+            this.tbLoginPass.Location = new System.Drawing.Point(263, 162);
+            this.tbLoginPass.Name = "tbLoginPass";
+            this.tbLoginPass.Size = new System.Drawing.Size(176, 23);
+            this.tbLoginPass.TabIndex = 3;
             // 
-            // textBox2
+            // tbLoginStudentNum
             // 
-            this.textBox2.Location = new System.Drawing.Point(263, 91);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(176, 23);
-            this.textBox2.TabIndex = 4;
+            this.tbLoginStudentNum.Location = new System.Drawing.Point(263, 91);
+            this.tbLoginStudentNum.Name = "tbLoginStudentNum";
+            this.tbLoginStudentNum.Size = new System.Drawing.Size(176, 23);
+            this.tbLoginStudentNum.TabIndex = 4;
             // 
-            // label1
+            // lblLoginStudentNum
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(263, 67);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 21);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
+            this.lblLoginStudentNum.AutoSize = true;
+            this.lblLoginStudentNum.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLoginStudentNum.Location = new System.Drawing.Point(263, 67);
+            this.lblLoginStudentNum.Name = "lblLoginStudentNum";
+            this.lblLoginStudentNum.Size = new System.Drawing.Size(128, 21);
+            this.lblLoginStudentNum.TabIndex = 5;
+            this.lblLoginStudentNum.Text = "Student Number:";
             // 
-            // pictureBox2
+            // pbLoginPic2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(454, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(231, 247);
-            this.pictureBox2.TabIndex = 6;
-            this.pictureBox2.TabStop = false;
+            this.pbLoginPic2.Image = global::Mealz_Demo.Properties.Resources.Mealz_logos__2_;
+            this.pbLoginPic2.Location = new System.Drawing.Point(454, 12);
+            this.pbLoginPic2.Name = "pbLoginPic2";
+            this.pbLoginPic2.Size = new System.Drawing.Size(231, 247);
+            this.pbLoginPic2.TabIndex = 6;
+            this.pbLoginPic2.TabStop = false;
             // 
-            // label2
+            // lblLoginPass
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(263, 138);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 21);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.lblLoginPass.AutoSize = true;
+            this.lblLoginPass.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLoginPass.Location = new System.Drawing.Point(263, 138);
+            this.lblLoginPass.Name = "lblLoginPass";
+            this.lblLoginPass.Size = new System.Drawing.Size(79, 21);
+            this.lblLoginPass.TabIndex = 7;
+            this.lblLoginPass.Text = "Password:";
+            this.lblLoginPass.Click += new System.EventHandler(this.label2_Click);
             // 
-            // label3
+            // lblLoginForgetPass
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(211, 373);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 21);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "label3";
+            this.lblLoginForgetPass.AutoSize = true;
+            this.lblLoginForgetPass.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLoginForgetPass.Location = new System.Drawing.Point(141, 373);
+            this.lblLoginForgetPass.Name = "lblLoginForgetPass";
+            this.lblLoginForgetPass.Size = new System.Drawing.Size(125, 21);
+            this.lblLoginForgetPass.TabIndex = 8;
+            this.lblLoginForgetPass.Text = "Forget Password";
             // 
-            // label4
+            // lblLoginCreateAcc
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(435, 373);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(52, 21);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "label4";
+            this.lblLoginCreateAcc.AutoSize = true;
+            this.lblLoginCreateAcc.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLoginCreateAcc.Location = new System.Drawing.Point(435, 373);
+            this.lblLoginCreateAcc.Name = "lblLoginCreateAcc";
+            this.lblLoginCreateAcc.Size = new System.Drawing.Size(151, 21);
+            this.lblLoginCreateAcc.TabIndex = 9;
+            this.lblLoginCreateAcc.Text = "Create New Account";
             // 
             // Form1
             // 
@@ -131,21 +134,21 @@ namespace Mealz_Demo
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HotTrack;
             this.ClientSize = new System.Drawing.Size(697, 495);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblLoginCreateAcc);
+            this.Controls.Add(this.lblLoginForgetPass);
+            this.Controls.Add(this.lblLoginPass);
+            this.Controls.Add(this.pbLoginPic2);
+            this.Controls.Add(this.lblLoginStudentNum);
+            this.Controls.Add(this.tbLoginStudentNum);
+            this.Controls.Add(this.tbLoginPass);
+            this.Controls.Add(this.pbLoginPic1);
+            this.Controls.Add(this.btnLogin);
             this.ForeColor = System.Drawing.SystemColors.InactiveBorder;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLoginPic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLoginPic2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,14 +156,14 @@ namespace Mealz_Demo
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox pbLoginPic1;
+        private System.Windows.Forms.TextBox tbLoginPass;
+        private System.Windows.Forms.TextBox tbLoginStudentNum;
+        private System.Windows.Forms.Label lblLoginStudentNum;
+        private System.Windows.Forms.PictureBox pbLoginPic2;
+        private System.Windows.Forms.Label lblLoginPass;
+        private System.Windows.Forms.Label lblLoginForgetPass;
+        private System.Windows.Forms.Label lblLoginCreateAcc;
     }
 }
